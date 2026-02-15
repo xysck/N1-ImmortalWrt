37714849cbda05989805328465426b5e79bba2c2
xysck <xysck@126.com>
2026-02-15 12:15:17 +0800

    Update diy.sh

--- diy/diy.sh
+++ diy/diy.sh
@@ -5,7 +5,10 @@ patch -p1 -f < $(dirname "$0")/luci.patch
 
 # Add packages
 git clone https://github.com/ophub/luci-app-amlogic --depth=1 clone/amlogic
+git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-adguardhome
 git clone https://github.com/Openwrt-Passwall/openwrt-passwall --depth=1 clone/passwall
+git clone --depth=1 https://github.com/DustReliant/luci-app-filetransfer package/luci-app-filetransfer
+git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-fileassistant
 
 # Update packages
 rm -rf feeds/luci/applications/luci-app-passwall
@@ -14,3 +17,23 @@ sed -i '/luci-app-attendedsysupgrade/d' feeds/luci/collections/luci/Makefile
 
 # Clean packages
 rm -rf clone
+
+# 修改默认IP、默认主机名、默认时区
+sed -i 's/192.168.1.1/192.168.2.254/g' package/base-files/files/bin/config_generate
+sed -i 's/ImmortalWrt/OpenWrt/g' package/base-files/files/bin/config_generate
+sed -i 's/UTC/Asia\/Shanghai/g' package/base-files/files/bin/config_generate
+
+# 修改默认主题
+sed -i 's/luci-theme-design/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+
+# 调整 zerotier 到 服务 菜单
+sed -i 's/vpn/services/g' feeds/luci/applications/luci-app-zerotier/root/usr/share/luci/menu.d/luci-app-zerotier.json
+
+# 添加 OpenClash Meta 内核
+mkdir -p files/etc/openclash/core
+wget -qO "clash_meta.tar.gz" "https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-arm64.tar.gz"
+tar -zxvf "clash_meta.tar.gz" -C files/etc/openclash/core/
+mv files/etc/openclash/core/clash files/etc/openclash/core/clash_meta
+chmod +x files/etc/openclash/core/clash_meta
+rm -f "clash_meta.tar.gz"
